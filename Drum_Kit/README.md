61aff885eb2f50937317148795e3ca35bbb7a31c
Alok Bhatt <72256788+bhattalok49@users.noreply.github.com>
2021-10-07 12:48:11 +0530

    Add files via upload

---
+++ Drum_Kit/README.md
@@ -0,0 +1,3 @@
+# Drum_Kit
+
+A It maps the keys A through L on the keyboard to a corresponding drum sound effect, & shows a visual key as feedback for the user.
---
+++ Drum_Kit/index.js
@@ -0,0 +1,60 @@
+var numOfDurmButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfDurmButtons; i++) {
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function handleCliked() {
+      var clickedButton = this.innerHTML;
+      makeSound(clickedButton);
+      buttonAnimation(clickedButton);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("assets/sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("assets/sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("assets/sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("assets/sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("assets/sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("assets/sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("assets/sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    default:
+      console.log();
+  }
+}
+
+function buttonAnimation(currentkey) {
+  var activeButton = document.querySelector("." + currentkey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
